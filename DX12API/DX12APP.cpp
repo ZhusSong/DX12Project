ed8ce93651ecaea3c439910e4ff4f8084ad3c097
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2024-05-13 17:44:23 +0900

    24.5.13
    创建DX12App

--- DX12API/DX12APP.cpp
+++ DX12API/DX12APP.cpp
@@ -1,6 +1,10 @@
 #include "DX12App.h"
+#include <WindowsX.h>
 #include <sstream>
+
 #pragma warning(disable: 6031)
+using namespace std;
+using namespace DirectX;
 extern "C"
 {
     // 在具有多显卡的硬件设备中，优先使用NVIDIA或AMD的显卡运行
@@ -8,6 +12,7 @@ extern "C"
     __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
     __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 0x00000001;
 }
+//Im
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 namespace
 {
@@ -22,18 +27,117 @@ MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     // Forward hwnd on because we can get messages (e.g., WM_CREATE)
     // before CreateWindow returns, and thus before m_hMainWnd is valid.
-    return g_d3dApp->MsgProc(hwnd, msg, wParam, lParam);
+    return DX12App::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+}
+
+DX12App* DX12App::mApp = nullptr;
+DX12App*DX12App::GetApp()
+{
+    return mApp;
 }
 
 DX12App::DX12App(HINSTANCE hInstance, const std::wstring& windowName, int initWidth, int initHeight)
-    : m_AppInstance(hInstance),
-    m_MainWndName(windowName),
-    m_ViewWidth(initWidth),
-    m_ViewHeight(initHeight),
-    m_MainWnd(nullptr),
 {
+    //单例，全局只能存在一个
+    // Only one D3DApp can be constructed.
+    assert(mApp == nullptr);
+    mApp = this;
+}
+DX12App::~DX12App()
+{
+    if (md3dDevice != nullptr)
+        FlushCommandQueue();
+}
+HINSTANCE DX12App::AppInstance()const
+{
+    return mhAppInstance;
+}
+HWND DX12App::MainWnd()const
+{
+    return mhMainWnd;
+}
+float DX12App::AspectRatio()const
+{
+    return static_cast<float>(mClientWidth) / mClientHeight;
+}
+bool  DX12App::Get4xMsaaState()const
+{
+    return m4xMsaaState;
+}
+void  DX12App::Set4xMsaaState(bool value)
+{
+    if (m4xMsaaState != value)
+    {
+        m4xMsaaState = value;
+        // Recreate the swapchain and buffers with new multisample settings.
+        //重设交换链与缓冲区
+        CreateSwapChain();
+        OnResize();
+    }
+}
+int DX12App::Run()
+{
+    MSG msg = { 0 };
+    mTimer.Reset();
+    while (msg.message != WM_QUIT)
+    {
+        // If there are Window messages then process them.
+        // 若有窗口信息则先处理窗口信息
+        if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        // Otherwise, do animation/game stuff.
+        // 若无窗口信息则处理游戏进程
+        else
+        {
+            mTimer.GetTick();
+            //若当前程序未暂停则进行更新与绘制
+            if (!mAppPaused)
+            {
+                ShowFrameCount();
+                Update(mTimer);
+                Draw(mTimer);
+            }
+            else
+            {
+                Sleep(100);
+            }
+        }
+    }
+    return (int)msg.wParam;
+}
+bool DX12App::Init()
+{
+    if (!InitMainWindow())
+        return false;
 
-    // 让一个全局指针获取这个类，这样我们就可以在Windows消息处理的回调函数
-    // 让这个类调用内部的回调函数了
-    g_d3dApp = this;
+    if (!InitDirect3D())
+        return false;
+
+    // Do the initial resize code.
+    // 按照初始宽高比进行创建
+    OnResize();
+
+    return true;
+}
+void DX12App::CreateRtvAndDsvDescriptorHeaps()
+{
+    D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
+    rtvHeapDesc.NumDescriptors = SwapChainBufferCount;
+    rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    rtvHeapDesc.NodeMask = 0;
+    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
+        &rtvHeapDesc, IID_PPV_ARGS(mRtvHeap.GetAddressOf())));
+
+
+    D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
+    dsvHeapDesc.NumDescriptors = 1;
+    dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+    dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    dsvHeapDesc.NodeMask = 0;
+    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
+        &dsvHeapDesc, IID_PPV_ARGS(mDsvHeap.GetAddressOf())));
 }
--- DX12API/DX12App.h
+++ DX12API/DX12App.h
@@ -26,86 +26,167 @@
 
 class DX12App
 {
-public:
-    //初始化
+    protected:
+     //初始化,并设置单例
      //Initialisation
      //初期化
     DX12App(HINSTANCE hInstance, const std::wstring& windowName, int initWidth, int initHeight);
+    //禁止编译器使用拷贝构造函数创建DX12App的拷贝
+    DX12App(const DX12App& rhs) = delete;
+    //禁止编译器通过使用=来创建DX12App的拷贝
+    DX12App& operator=(const DX12App& rhs) = delete;
 
+    //释放所有ComPtr对象，使用构造函数进行销毁是为了保证在销毁之前，GPU能处理完所有指令
     virtual ~DX12App();
+public:
+    //单例 返回DX12App实例
+    static DX12App* GetApp();
 
     //获取应用实例的句柄
     HINSTANCE AppInstance()const;
     //获取主窗口句柄
     HWND      MainWnd()const;
     //获取屏幕宽高比
-    float     FormRatio()const;
+    float    AspectRatio()const;
+
+    //是否使用4X MSAA功能
+    bool Get4xMsaaState()const;
+    //开启或禁用4X MSAA功能
+    void Set4xMsaaState(bool value);
 
     //运行
     int Run();
 
-    //初始化窗口及Direct3D部分
+    //初始化窗口及Direct3D部分,用于在派生类中进行初始化
     virtual bool Init();
-    //在窗口大小变动时调用
-    virtual void OnResize();
-    //实现每一帧更新
-    virtual void UpdateScene(float dt) = 0;
-    //实现每一帧绘制
-    virtual void DrawScene() = 0;
     //窗口消息回调
     virtual LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+protected:
+    //创建RTV与DSV描述符
+    virtual void CreateRtvAndDsvDescriptorHeaps();
+
+    //在窗口大小变动时调用，获取变化后的屏幕宽高比并进行后台缓冲区的重新创建
+    virtual void OnResize();
+
+    //实现每一帧更新
+    virtual void Update(const DXGameTimer& gt) = 0;
+
+    //实现每一帧绘制
+    virtual void Draw(const DXGameTimer& gt) = 0;
+
+
+    //使用ImGui进行输入检测，此处不需要
+    // Convenience overrides for handling mouse input.
+    //virtual void OnMouseDown(WPARAM btnState, int x, int y) { }
+    //virtual void OnMouseUp(WPARAM btnState, int x, int y) { }
+    //virtual void OnMouseMove(WPARAM btnState, int x, int y) { }
 
     //鼠标位置测试
     float m_MousePosX;
     float m_MousePosY;
 
-
-protected:
-    //窗口初始化
+    //初始化主窗口
     bool InitMainWindow();
     // Direct2D初始化
     bool InitDirect2D();
     //Direct3D初始化
     bool InitDirect3D();
+    //创建命令队列，命令列表分配器与命令列表
+    void CreateCommandObjects();
 
-    bool InitImGui();
+    //创建交换链
+    void CreateSwapChain();
+
+    //强制CPU等待GPU，直到GPU处理完队列中所有命令
+    void FlushCommandQueue();
+    //返回交换链中当前后台缓冲区的ID3D12Resource
+    ID3D12Resource* CurrentBackBuffer()const;
+    //返回当前后台缓冲区的RTV(渲染目标视图，render target view)
+    D3D12_CPU_DESCRIPTOR_HANDLE CurrentBackBufferView()const;
+    //返回主深度/模板缓冲区的DSV(深度/模板视图，depth/stencil view)
+    D3D12_CPU_DESCRIPTOR_HANDLE DepthStencilView()const;
+
+
     //显示帧数
     void ShowFrameCount();
-
+    //枚举系统中所有适配器
+    void LogAdapters();
+    //枚举指定适配器的全部显示输出
+    void LogAdapterOutputs(IDXGIAdapter* adapter);
+    //枚举某个显示输出对特定格式支持的所有显示模式
+    void LogOutputDisplayModes(IDXGIOutput* output, DXGI_FORMAT format);
+    //初始化ImGui
+    bool InitImGui();
 protected:
-    //窗口初始化
-    bool InitMainWindow();
-    // Direct2D初始化
-    bool InitDirect2D();
-    //Direct3D初始化
-    bool InitDirect3D();
 
-    bool InitImGui();
-    //显示帧数
-    void ShowFrameCount();
+    static DX12App* mApp;
 
     //应用实例句柄
-    HINSTANCE m_AppInstance;
+    HINSTANCE mhAppInstance = nullptr;
     //主窗口
-    HWND      m_MainWnd;
+    HWND      mhMainWnd = nullptr;
     //是否暂停
-    bool      m_AppPaused;
+    bool      mAppPaused = false;
     //是否最小化
-    bool      m_Minimized;
+    bool      mMinimized = false;
     //是否最大化
-    bool      m_Maximized;
+    bool      mMaximized = false;
     //窗口大小是否变化
-    bool      m_Resizing;
+    bool      mResizing = false;
+    // fullscreen enabled
+    //是否全屏
+    bool      mFullscreenState = false;
     //是否开启4倍多重采样
-    bool      m_Enable4xMsaa;
+    bool      m4xMsaaState=false;
     //MSAA质量等级
-    UINT      m_4xMsaaQuality;
+    UINT      m4xMsaaQuality=0;
 
     //计时器
-    DXGameTimer m_Timer;
+    DXGameTimer mTimer;
 
     //模板
     template <class T>
     using ComPtr = Microsoft::WRL::ComPtr<T>;
+    //工厂
+    ComPtr<IDXGIFactory4> mdxgiFactory;
+    //交换链
+    ComPtr<IDXGISwapChain> mSwapChain;
+    //d3d设备
+    ComPtr<ID3D12Device> md3dDevice;
+
+    ComPtr<ID3D12Fence> mFence;
+    UINT64 mCurrentFence = 0;
+
+    ComPtr<ID3D12CommandQueue> mCommandQueue;
+    ComPtr<ID3D12CommandAllocator> mDirectCmdListAlloc;
+    ComPtr<ID3D12GraphicsCommandList> mCommandList;
+
+    static const int SwapChainBufferCount = 2;
+    int mCurrBackBuffer = 0;
+    ComPtr<ID3D12Resource> mSwapChainBuffer[SwapChainBufferCount];
+    ComPtr<ID3D12Resource> mDepthStencilBuffer;
+
+    ComPtr<ID3D12DescriptorHeap> mRtvHeap;
+    ComPtr<ID3D12DescriptorHeap> mDsvHeap;
+
+    D3D12_VIEWPORT mScreenViewport;
+    D3D12_RECT mScissorRect;
+
+    UINT mRtvDescriptorSize = 0;
+    UINT mDsvDescriptorSize = 0;
+    UINT mCbvSrvUavDescriptorSize = 0;
+
+    // Derived class should set these in derived constructor to customize starting values.
+    // 用户应在派生类的派生构造函数中定义这些初始值
+    //窗口名
+    std::wstring mMainWndCaption = L"d3d App";
+    //窗口宽度
+    int mClientWidth;
+    //窗口高度
+    int mClientHeight;
+    D3D_DRIVER_TYPE md3dDriverType = D3D_DRIVER_TYPE_HARDWARE;
+    DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+    DXGI_FORMAT mDepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
 };
 #endif
